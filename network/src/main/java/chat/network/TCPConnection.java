d60ffc6ac8a76f4372edd88171937381ae9da6ae
skyba <ksv@evogence.com>
2018-03-09 22:15:38 +0200

    added interface TPCConnectionListener

---
+++ network/src/main/java/chat/network/TCPConnection.java
@@ -0,0 +1,33 @@
+package chat.network;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+public class TCPConnection {
+    private final Socket socket;
+    private final Thread rxThread;
+    private  final TCPConnectionListener eventListener;
+    private final BufferedReader in;
+    private final BufferedWriter out;
+
+    public TCPConnection(TCPConnectionListener eventListener, Socket socket) throws IOException {
+        this.eventListener = eventListener;
+        this.socket = socket;
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream(), Charset.forName("UTF-8")));
+        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), Charset.forName("UTF-8")));
+        rxThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String mag = in.readLine();
+                }catch (IOException e){
+
+                }finally {
+
+                }
+            }
+        });
+        rxThread.start();
+    }
+}
